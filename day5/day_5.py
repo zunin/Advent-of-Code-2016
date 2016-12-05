f800026883276793dcec1ca9ecf900485434e85c
Nikolai Øllegaard <nikolai@oellegaard.dk>
2016-12-06 00:01:22 +0100

    day 5 part 2 added

--- day5/day_5.py
+++ day5/day_5.py
@@ -13,18 +13,24 @@ class InputParser(BaseInputParser):
 class MD5Hasher:
 
     def find_hash_with_prefix(self, id, prefix=0, prefix_length=5, password_length=8):
-        password = []
-
+        part_1_password = []
+        part_2_password = {}
         index = 0
-        while len(password) < password_length:
+        while len(part_1_password) < password_length or len(part_2_password) < password_length:
             id_plus_index = str(id)+str(index)
             hash_result = hashlib.md5(id_plus_index.encode()).hexdigest()
 
             if hash_result.startswith(prefix_length*str(prefix)):
-                password.append(hash_result[prefix_length])
+                if len(part_1_password) < password_length:
+                    part_1_password.append(hash_result[prefix_length])
+
+                password_2_index = int(hash_result[prefix_length], 16)
+                if len(part_2_password) < password_length and password_2_index < password_length and password_2_index not in part_2_password.keys():
+                    part_2_password[password_2_index] = hash_result[prefix_length + 1]
+
             index += 1
 
-        return "".join(password)
+        return part_1_password, part_2_password
 
 
 def main():
@@ -33,8 +39,10 @@ def main():
 
     hasher = MD5Hasher()
 
-    print(hasher.find_hash_with_prefix(id=secret))
-
+    solution_1, solution_2 = hasher.find_hash_with_prefix(id=secret)
+    print(solution_1, solution_2)
+    print("The password is %s" % "".join(solution_1))
+    print("The second door's password is %s" % "".join(solution_2.values()))
 
 if __name__ == '__main__':
     main()
