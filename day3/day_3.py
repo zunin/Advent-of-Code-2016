36a5f44826f691a837f9d78b8116f1a18122b2e6
Nikolai Øllegaard <nikolai@oellegaard.dk>
2016-12-05 21:10:16 +0100

    added day 3

---
+++ day3/day_3.py
@@ -0,0 +1,71 @@
+from collections import namedtuple
+
+from common import BaseInputParser
+
+
+class InputParser(BaseInputParser):
+    @property
+    def triangles(self):
+        for line in self.lines:
+            dimensions = line.split()
+            yield Triangle(a=dimensions[0], b=dimensions[1], c=dimensions[2])
+
+    @property
+    def triple_columns(self):
+        triplet = []
+        for line_number, line in enumerate(self.lines):
+            triplet.append(line.split())
+            if triplet and len(triplet) == 3:
+                yield triplet
+                triplet = []
+
+
+    @property
+    def column_triangles(self):
+        for triplet in self.triple_columns:
+            triangles = [
+                Triangle(a=triplet[0][0], b=triplet[1][0], c=triplet[2][0]),
+                Triangle(a=triplet[0][1], b=triplet[1][1], c=triplet[2][1]),
+                Triangle(a=triplet[0][2], b=triplet[1][2], c=triplet[2][2]),
+            ]
+            for triangle in triangles:
+                yield triangle
+
+
+class Triangle:
+    TriangleData = namedtuple("TriangleData", "a b c")
+
+    def __init__(self, a, b, c):
+        self.triangle = self.TriangleData(a=int(a), b=int(b), c=int(c))
+
+    def is_valid(self):
+        longest, middle, shortest = reversed(sorted(self.triangle))
+        return (shortest + middle) > longest
+
+    def __repr__(self):
+        return self.triangle.__repr__().replace(
+            self.TriangleData.__name__,
+            self.__class__.__name__
+        )
+
+
+def main():
+    parser = InputParser()
+
+    valid_triangles = 0
+    for triangle in parser.triangles:
+        if triangle.is_valid():
+            valid_triangles += 1
+
+    print("There are %s valid triangles on the walls" % valid_triangles)
+
+    valid_triangles = 0
+    for triangle in parser.column_triangles:
+        if triangle.is_valid():
+            valid_triangles += 1
+
+    print("There are %s valid triangles in columns on the walls" % valid_triangles)
+
+
+if __name__ == '__main__':
+    main()
