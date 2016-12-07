9aabaadbd6ca01003b628d183db9b0d19574d72a
Nikolai Øllegaard <nikolai@oellegaard.dk>
2016-12-07 08:58:59 +0100

    added part 2 of day 7

--- day7/day_7.py
+++ day7/day_7.py
@@ -11,21 +11,21 @@ class InputParser(BaseInputParser):
             raw_split = re.split("(\[[a-z]+\])", line.strip())
 
             hypernets = []
-            texts = []
+            supernets = []
             for line in raw_split:
                     if line.startswith("[") and line.endswith("]"):
                         hypernets.append(line[1:-1])
                     else:
-                        texts.append(line)
+                        supernets.append(line)
 
-            yield IPAddress(texts=texts, hypernets=hypernets)
+            yield IPAddress(supernets=supernets, hypernets=hypernets)
 
 
 class IPAddress:
     model = namedtuple("IPAddress", ["supernets", "hypernets"])
 
-    def __init__(self, texts, hypernets):
-        self.data = self.model(texts=texts, hypernets=hypernets)
+    def __init__(self, supernets, hypernets):
+        self.data = self.model(supernets=supernets, hypernets=hypernets)
 
     @property
     def supernets(self):
@@ -45,7 +45,7 @@ class IPAddress:
 class Validator:
     @staticmethod
     def has_abba(text):
-        for index, character in enumerate(text[3:]):
+        for index, _ in enumerate(text[3:]):
             first_pair = (text[index], text[index+1])
             second_pair = (text[index+2], text[index+3])
 
@@ -53,9 +53,22 @@ class Validator:
                 return True
         return False
 
+
+    @staticmethod
+    def get_abas(text: str):
+        abas = []
+        for index, _ in enumerate(text[2:]):
+            if text[index] == text[index+2] and text[index] != text[index+1]:
+                abas.append(text[index]+text[index+1]+text[index+2])
+        return abas
+
+    @staticmethod
+    def has_bab(aba: str, text: str):
+        bab = aba[1]+aba[0]+aba[1]
+        return bab in text
+
     @staticmethod
     def validate_with_abba(ip_address: IPAddress):
-        print(ip_address)
         for hypernet in ip_address.hypernets:
             if Validator.has_abba(hypernet):
                 return False
@@ -65,6 +78,19 @@ class Validator:
                 return True
 
         return False
+
+    @staticmethod
+    def validate_with_ssl(ip_address: IPAddress):
+        # if aba in supernet and bab in hypernet
+        all_abas = []
+        for supernet in ip_address.supernets:
+            abas = Validator.get_abas(supernet)
+            if abas:
+                all_abas += abas
+        for hypernet in ip_address.hypernets:
+            for aba in all_abas:
+                if Validator.has_bab(aba, hypernet):
+                    return True
 
 
 def main():
@@ -77,6 +103,12 @@ def main():
 
     print("There are %s ip addresses that support TLS according to ABBA validation" % tls_ip_addresses)
 
+    ssl_ip_addresses = 0
+    for address in parser.ip_addresses:
+        if Validator.validate_with_ssl(address):
+            ssl_ip_addresses += 1
+
+    print("There are %s ip addresses that support SSL" % ssl_ip_addresses)
 
 if __name__ == '__main__':
     main()
